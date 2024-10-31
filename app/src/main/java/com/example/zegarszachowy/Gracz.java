ddbbd273e88049fa8b41a72d3a8742c2573c3c42
JustSG <szymonfujawa.07@gmail.com>
2024-10-31 13:34:56 +0100

    skonczony projekt

--- app/src/main/java/com/example/zegarszachowy/Gracz.java
+++ app/src/main/java/com/example/zegarszachowy/Gracz.java
@@ -1,2 +1,44 @@
-package com.example.zegarszachowy;public class Gracz {
+package com.example.zegarszachowy;
+
+import android.os.CountDownTimer;
+import android.widget.Button;
+
+public class Gracz {
+    int liczbaSekund = 180;
+    boolean czyAktywny = false;
+    CountDownTimer countDownTimer;
+    Button button;
+    String czasDoWyswietlenia;
+    int sekundy;
+
+    public Gracz(boolean czyAktywny, Button button) {
+        this.czyAktywny = czyAktywny;
+        this.button = button;
+    }
+
+    public void liczCzas(){
+        czyAktywny = true;
+        countDownTimer = new CountDownTimer(liczbaSekund*1000, 1000) { // co będę liczył i jaki interwał \\ co 1000 interwałów uruchania się 1 tick zapisany jako l
+            @Override
+            public void onTick(long l) {
+                liczbaSekund = (int)(l/1000);
+                int s60 = liczbaSekund % 60;
+                int h60 = liczbaSekund / 3600;
+                int m60 = (liczbaSekund - h60 * 3600) / 60;
+                czasDoWyswietlenia = String.format("%02d:%02d",m60, s60);
+                button.setText(String.valueOf(czasDoWyswietlenia));
+            }
+
+            @Override
+            public void onFinish() {
+                button.setText("KONIEC");
+                button.setActivated(false);
+            }
+        };
+        countDownTimer.start();
+    }
+    public void stopCzas(){
+        czyAktywny = false;
+        countDownTimer.cancel();
+    }
 }
--- app/src/main/java/com/example/zegarszachowy/MainActivity.java
+++ app/src/main/java/com/example/zegarszachowy/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.zegarszachowy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1;
+    Button button2;
+    private Gracz gracz1;
+    private Gracz gracz2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+        gracz1 = new Gracz(true, button1);
+        gracz1.liczCzas();
+        gracz2 = new Gracz(false, button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(gracz1.czyAktywny) {
+                    gracz2.liczCzas();
+                    gracz1.stopCzas();
+                }
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(gracz2.czyAktywny) {
+                    gracz1.liczCzas();
+                    gracz2.stopCzas();
+                }
+            }
+        });
     }
 }
